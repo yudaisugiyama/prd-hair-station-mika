1d3f4b6e8548bac38daf5fb8bca964b9451dacd6
yudai <sugi.nut@gmail.com>
2023-06-22 16:52:58 +0900

    Update.

---
+++ src-3.0/mika-app/app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: '新潟駅前美容院  |  Hair Station Mika（ヘアーステーションミカ）',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="jp">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
