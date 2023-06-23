18ca356546abeb9dcb4acb95c23e3d322e16f959
yudai <sugi.nut@gmail.com>
2023-06-23 17:54:16 +0900

    feat: ✨ Update pages dor.

--- src-3.2/pages/_app.tsx
+++ src-3.2/pages/_app.tsx
@@ -1,8 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '新潟駅前美容院  |  Hair Station Mika（ヘアーステーションミカ）',
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
