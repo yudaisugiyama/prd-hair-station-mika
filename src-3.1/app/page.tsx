909df2b0b941832652485b01b1e5b55012820854
yudai <sugi.nut@gmail.com>
2023-06-23 14:38:28 +0900

    3.1 💩 Nextjs13,TypeScript,Nginx

--- src-3.1/app/page.tsx
+++ src-3.1/app/page.tsx
@@ -1,8 +1,35 @@
-import Image from 'next/image'
+import { useClient } from 'next/client'
+import { useRef } from 'react'
 import styles from './page.module.css'
 
 export default function Home() {
+  const { isClient } = useClient()
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const handleRestart = () => {
+    const video = videoRef.current
+    if (video) {
+      video.currentTime = 0
+      video.play()
+    }
+  }
+
   return (
-    <h1>Hi there</h1>
+    <div className={styles.center}>
+      <video width={640} height={360} controls>
+        <source src="/video/movie.mp4" type="video/mp4" />
+      </video>
+      {isClient && (
+        <video
+          className="video"
+          src="assets/video/eyecatch-movie.mp4"
+          autoPlay
+          muted
+          playsInline
+          onEnded={handleRestart}
+          ref={videoRef}
+        />
+      )}
+    </div>
   )
 }
